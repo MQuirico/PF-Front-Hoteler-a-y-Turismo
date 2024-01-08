8027daa832049ab5763dccefe62c8edad4f8ab7b
AndresveraXX <andres_9_09@hotmail.com>
2024-01-08 20:14:40 -0300

    create

---
+++ src/views/Home/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  // Contenido del componente Detail
+
+  return (
+    <div>
+      {/* Contenido del componente Detail */}
+    </div>
+  );
+};
+
+export default Home; 
