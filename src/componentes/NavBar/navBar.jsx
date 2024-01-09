521cfe62dd538385f30038ded82a727384dbc272
MQuirico <128657405+MQuirico@users.noreply.github.com>
2024-01-08 22:25:05 -0300

    segundo commit Matias

--- src/componentes/NavBar/navBar.jsx
+++ src/componentes/NavBar/navBar.jsx
@@ -5,6 +5,7 @@ export default function navBar (props) {
         <div>
         <h1>RunnersParadise</h1>
         <h3>Home</h3>
+        <h3>Nuestros productos</h3>
         <h3>¿Quiénes somos?</h3>
         <h3>Registrarse/Iniciar sesión</h3>
         </div>
