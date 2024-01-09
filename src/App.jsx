6a738da2868247613852fc0858d02e38e6b91e37
Abril31 <brivaldi0416@gmail.com>
2024-01-08 22:12:27 -0400

    [FIX] card y cards. Ahora se renderizan en Home.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './views/Home/home';
-import Detail from './views/Detail/detail';
-import Create from './componentes/Create/create';
-import Landing from './views/Landing/landing';
-import { Provider } from 'react-redux';
-import store from "./redux/store/store"
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./views/Home/Home";
+import Detail from "./views/Detail/Detail";
+import Create from "./componentes/Create/Create";
+import Landing from "./views/Landing/landing";
+import { Provider } from "react-redux";
+import store from "./redux/store/store";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
