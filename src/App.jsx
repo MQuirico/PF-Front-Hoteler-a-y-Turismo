eb80b168e1feb7ca76d753244a52de67b8685558
MQuirico <128657405+MQuirico@users.noreply.github.com>
2024-01-11 00:26:48 -0300

    ultimo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./views/Home/home";
-import Detail from "./views/Detail/detail";
-import Create from "./componentes/Create/create";
-import Landing from "./views/Landing/landing";
-import LogIn from "./componentes/LogIn Form/LogIn";
-import About from "./componentes/About/about";
-import Registro from "./componentes/Register/Register";
-import { Provider } from "react-redux";
-import store from "./redux/store/store";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/home" component={Home} />
-            <Route exact path="/detail/:id" component={Detail} />
-            <Route exact path="/create" component={Create} />
-            <Route exact path="/login" component={LogIn} />
-            <Route exact path="/about" component={About} />
-            <Route exact path='/register' component={Registro} />
-          </Switch>
-        </div>
-      </Router>
-    </Provider>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
