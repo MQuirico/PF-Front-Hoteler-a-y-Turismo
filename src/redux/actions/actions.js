ed4b17ee555ff0f6f6ade6a611c91aa6a874c773
tomaspon <tomaspon11@gmail.com>
2024-01-13 14:29:34 -0300

    feature(client/redux - files order)

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,27 +1,29 @@
-
-export const POST_PRODUCT_REQUEST = 'POST_PRODUCT_REQUEST';
-export const POST_PRODUCT_SUCCESS = 'POST_PRODUCT_SUCCESS';
-export const POST_PRODUCT_FAILURE = 'POST_PRODUCT_FAILURE';
-export const FETCH_PRODUCT_DETAIL_SUCCESS = "FETCH_PRODUCT_DETAIL_SUCCESS";
-export const FETCH_PRODUCT_DETAIL_FAILURE = "FETCH_PRODUCT_DETAIL_FAILURE";
-export const CLEAR_PRODUCT_DETAIL = "CLEAR_PRODUCT_DETAIL";
-export const CREATE_PRODUCT_REQUEST = "CREATE_PRODUCT_REQUEST";
-export const CREATE_PRODUCT_SUCCESS = "CREATE_PRODUCT_SUCCESS";
-export const CREATE_PRODUCT_FAILURE = "CREATE_PRODUCT_FAILURE";
-export const CLEAR_CREATE_PRODUCT_STATE = "CLEAR_CREATE_PRODUCT_STATE";
-export const CREATE_USER_REQUEST = "CREATE_USER_REQUEST";
-export const CREATE_USER_SUCCESS = "CREATE_USER_SUCCESS";
-export const CREATE_USER_FAILURE = "CREATE_USER_FAILURE";
 import axios from "axios";
-import { GET_ALL_SNEAKERS,GET_SEARCH_REQUEST, GET_SEARCH_NOTFOUND, GET_SEARCH_SUCCESS } from "../action-types";
+import {
+  GET_ALL_SNEAKERS,
+  GET_SEARCH_REQUEST,
+  GET_SEARCH_NOTFOUND,
+  GET_SEARCH_SUCCESS,
+  POST_PRODUCT_FAILURE,
+  POST_PRODUCT_SUCCESS,
+  POST_PRODUCT_REQUEST,
+  CREATE_PRODUCT_FAILURE,
+  CREATE_PRODUCT_SUCCESS,
+  CREATE_PRODUCT_REQUEST,
+  CREATE_USER_REQUEST,
+  CREATE_USER_SUCCESS,
+  CREATE_USER_FAILURE,
+  CLEAR_PRODUCT_DETAIL,
+  CLEAR_CREATE_PRODUCT_STATE
+} from "../action-types/action-types";
 
 export const registerUser = (datauser) => async (dispatch) => {
-  dispatch({ type: 'CREATE_USER_REQUEST' });
+  dispatch({ type: CREATE_USER_REQUEST });
   try {
     const response = await axios.post('http://localhost:3000/users/create', datauser);
-    dispatch({ type: 'CREATE_USER_SUCCESS', payload: response.data });
+    dispatch({ type: CREATE_USER_SUCCESS, payload: response.data });
   } catch (error) {
-    dispatch({ type: 'CREATE_USER_FAILURE', payload: error.message });
+    dispatch({ type: CREATE_USER_FAILURE, payload: error.message });
   }
 }
 
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,4 +1,8 @@
 import {
+  GET_ALL_SNEAKERS,
+  GET_SEARCH_REQUEST,
+  GET_SEARCH_SUCCESS,
+  GET_SEARCH_NOTFOUND,
   POST_PRODUCT_REQUEST,
   POST_PRODUCT_SUCCESS,
   POST_PRODUCT_FAILURE,
@@ -8,10 +12,8 @@ import {
   CREATE_PRODUCT_REQUEST,
   CREATE_PRODUCT_SUCCESS,
   CREATE_PRODUCT_FAILURE,
-  CLEAR_CREATE_PRODUCT_STATE,
-  
-} from "../actions/actions";
-import { GET_ALL_SNEAKERS, GET_SEARCH_REQUEST, GET_SEARCH_SUCCESS, GET_SEARCH_NOTFOUND } from "../action-types/index";
+  CLEAR_CREATE_PRODUCT_STATE
+} from "../action-types/action-types";
 
 const initialState = {
   loading: false,
@@ -22,7 +24,6 @@ const initialState = {
     error: null,
   },
   error: null,
-  
   sneakers: []
 };
 
@@ -62,81 +63,82 @@ const productReducer = (state = initialState, action) => {
         ...state,
         sneakers: action.payload,
       };
-      case FETCH_PRODUCT_DETAIL_SUCCESS:
-        console.log("Detalle del producto:", action.payload);
-        return {
-            ...state,
-            product: {
-                ...state.product,
-                detail: action.payload,
-            },
-            error: null,
-        };
-        case FETCH_PRODUCT_DETAIL_FAILURE:
-          return {
-            ...state,
-            product: {
+
+    case FETCH_PRODUCT_DETAIL_SUCCESS:
+      console.log("Detalle del producto:", action.payload);
+      return {
+          ...state,
+          product: {
               ...state.product,
-              detail: null,
-            },
-            error: action.payload,
-          };
+              detail: action.payload,
+          },
+          error: null,
+      };
 
-          case CLEAR_PRODUCT_DETAIL:
- return {
-   ...state,
-   product: {
-     ...state.product,
-     detail: null,
-   },
- };
+    case FETCH_PRODUCT_DETAIL_FAILURE:
+      return {
+        ...state,
+        product: {
+          ...state.product,
+          detail: null,
+        },
+        error: action.payload,
+      };
 
- case CREATE_PRODUCT_REQUEST:
-  return {
-    ...state,
-    loading: true,
-    error: null,
-  };
+    case CLEAR_PRODUCT_DETAIL:
+      return {
+        ...state,
+        product: {
+          ...state.product,
+          detail: null,
+        },
+      };
 
-  case CREATE_PRODUCT_SUCCESS:
-    return {
-      ...state,
-      loading: false,
-      product: {
-        ...state.product,
-        createdProduct: action.payload,
-      },
-      error: null,
-    };
+    case CREATE_PRODUCT_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
 
-case CREATE_PRODUCT_FAILURE:
-  return {
-    ...state,
-    loading: false,
-    createdProduct: null,
-    error: action.payload,
-  };
+    case CREATE_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        product: {
+          ...state.product,
+          createdProduct: action.payload,
+        },
+        error: null,
+      };
 
-case CLEAR_CREATE_PRODUCT_STATE:
-  return initialState;
+    case CREATE_PRODUCT_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        createdProduct: null,
+        error: action.payload,
+      };
+
+    case CLEAR_CREATE_PRODUCT_STATE:
+      return initialState;
 
 
-  case GET_SEARCH_SUCCESS:
-    return{
-      ...state,
-      loading: false,
-      data: action.payload,
-      error: null,
-    };
+    case GET_SEARCH_SUCCESS:
+      return{
+        ...state,
+        loading: false,
+        data: action.payload,
+        error: null,
+      };
 
-  case GET_SEARCH_NOTFOUND:
-    return{
-      ...state,
-      loading: false,
-      data: null,
-      error: action.payload
-    };
-
+    case GET_SEARCH_NOTFOUND:
+      return{
+        ...state,
+        loading: false,
+        data: null,
+        error: action.payload
+      };
   
     default:
       return state;
