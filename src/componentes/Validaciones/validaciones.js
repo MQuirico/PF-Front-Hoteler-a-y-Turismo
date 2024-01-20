9034f332f74c7ab8fffc38624c491a68f7e542f5
AndresveraXX <andres_9_09@hotmail.com>
2024-01-20 18:46:39 -0300

    fix buttom bar

--- src/componentes/Validaciones/validaciones.js
+++ src/componentes/Validaciones/validaciones.js
@@ -17,7 +17,36 @@ const validation = (input, existingNames) => {
     if (isNaN(parseFloat(input.price)) || parseFloat(input.price) < 1 || parseFloat(input.price) > 10000) {
         errors.price = "Ingrese un precio entre 1 y 10000";
      }
-  
-    return errors;
-   }
-   export default validation;
+
+     const validation = (input, existingNames) => {
+        let errors = {};
+       
+        let noEmpty = /\S+/;
+        let validateName = /^[a-zA-ZñÑ\s]*$/; // Permitir espacios en blanco en el nombre
+      
+        if (Array.isArray(existingNames) && existingNames.some((name) => name.toLowerCase() === input.name.toLowerCase())) {
+         errors.name = "Este nombre ya está en uso. Por favor, elige otro.";
+        } else if (!noEmpty.test(input.name) ? !validateName.test(input.name) : input.name.trim().length < 3) {
+         errors.name = "Nombre necesario. Mayor de 3 letras y único";
+        }
+      
+        if (!(input.image instanceof File)) {
+         errors.image = "Debe ser un archivo válido";
+        }
+      
+        if (isNaN(parseFloat(input.price)) || parseFloat(input.price) < 1 || parseFloat(input.price) > 10000) {
+            errors.price = "Ingrese un precio entre 1 y 10000";
+         }
+    
+        if (!Array.isArray(input.color) || input.color.length < 2) {
+            errors.color = "Selecciona al menos dos colores";
+        }
+    
+        if (!Array.isArray(input.size) || input.size.length < 2) {
+            errors.size = "Selecciona al menos dos tamaños";
+        }
+      
+        return errors;
+    }
+}
+export default validation;
