16a0afd723d4c553704fca6f3d0be5374b4e0182
AAlexandraCastillo <casstlealexandra@gmail.com>
2024-02-07 21:29:52 -0300

    Landing

---
+++ src/componentes/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,35 @@
+import  { useState } from 'react';
+
+import style from './ImageGallery.module.css'
+const ImageGallery = ({ images }) => {
+  const [startIndex, setStartIndex] = useState(0); 
+
+  const handleNext = () => {
+    if (startIndex + 4 < images.length) {
+      setStartIndex(startIndex + 4);
+    }
+  };
+
+  const handlePrev = () => {
+    
+    if (startIndex - 4 >= 0) {
+      setStartIndex(startIndex - 4);
+    }
+  };
+
+  return (
+    <section className={style.container}>
+     {startIndex ===0?'': <button onClick={handlePrev} className={style.boton} >Ant</button>}
+      {images.slice(startIndex, startIndex + 4).map((imageUrl, index) => (
+        <img
+          key={index}
+          src={imageUrl}
+          alt={`Imagen${startIndex + index + 1}`}
+        />
+      ))}
+   <button onClick={handleNext} className={style.boton} disabled={startIndex + 4 >= images.length}>Sig</button>
+    </section>
+  );
+}
+
+export default ImageGallery;
---
+++ src/componentes/Landing/Landing.jsx
@@ -0,0 +1,33 @@
+
+import style from './Landing.module.css'
+import ImageGallery from '../Images/ImageGallery' 
+const Landing=()=>{
+ const images=['https://tse1.mm.bing.net/th?id=OIP.gy0hB8b8-0YvJ3ODBAql6QHaEo&pid=Api&P=0&h=180','https://image.freepik.com/foto-gratis/amigos-divirtiendose-alrededor-fogata-noche_109710-4520.jpg','https://tse3.mm.bing.net/th?id=OIP.1-a6tn9mc0gwS1c2OfR-9gHaE7&pid=Api&P=0&h=180','https://tse1.mm.bing.net/th?id=OIP.RP5Eh4uosoD6ZgnX2cqDAwHaFj&pid=Api&P=0&h=180','https://www.interpatagonia.com/plantillas/grandes/33579-00Gr.jpg','https://casasrusticas.org/wp-content/uploads/2021/12/cabanas-rusticas-de-piedra-y-madera-01-1024x683.jpg','https://images.mirai.com/INFOROOMS/100120276/09Tb4L1tXTdhiaVkF2UT/09Tb4L1tXTdhiaVkF2UT_large.jpg','https://i.pinimg.com/originals/ce/8d/fe/ce8dfe3b7a548e752b95085e8766e551.jpg']
+     return(
+       <div className={style.cajita}>
+          <button className={style.miboton}>Comenzar</button>    
+          <header className={style.hed}>
+           <h1>¡Descubre tu escape perfecto!</h1>
+          <h2>Reserva tu cabaña ideal para unas vacaciones inolvidables</h2>
+          <p>Explora nuestro oasis de paz y tranquilidad en medio de la naturaleza.¡Bienvenido a tu refugio vacacional!</p>
+         </header>
+
+<ImageGallery images={images}/>
+<p className={style.parrafo}>Además de disfrutar de nuestras cómodas cabañas, puedes explorar la belleza natural de la zona, hacer senderismo en los senderos cercanos, o disfrutar de una cena en los restaurantes locales.</p>
+<footer className={style.foot}>
+ <div>
+    <div class="row">
+      <div class="col-md-12">
+        <p>Derechos de autor © 2024. Todos los derechos reservados.        </p>
+        <h1> .contactanos:  0800-345-678</h1>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+
+
+             </div>)
+}
+export default Landing;
