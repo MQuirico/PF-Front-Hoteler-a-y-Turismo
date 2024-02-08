8047ea5a5ee0dafe532557fcaa4e5f0f44b9b2aa
MQuirico <128657405+MQuirico@users.noreply.github.com>
2024-02-08 20:46:11 -0300

    correccion

--- src/redux/Actions/actions.js
+++ src/redux/Actions/actions.js
@@ -1,1 +1,57 @@
-//dsadsa
+//dsadsaimport axios from "axios";
+import {
+    SET_USER_DATA,
+    CLEAR_USER_DATA,
+    CREATE_USER_REQUEST,
+    CREATE_USER_SUCCESS,
+    CREATE_USER_FAILURE,
+    GET_SEARCH_BY_NAME,
+    NO_EVENTS
+
+
+} from "../Actions_Type/actions_type";
+
+export const setUserData = (userData) => {
+    return {
+      type: SET_USER_DATA,
+      payload: userData,
+    };
+  };
+  
+  export const clearUserData = () => {
+    return {
+      type: CLEAR_USER_DATA,
+    };
+  };
+
+  export const registerUser = (datauser) => async (dispatch) => {
+    dispatch({ type: CREATE_USER_REQUEST });
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/users/create",
+        datauser
+      );
+      dispatch({ type: CREATE_USER_SUCCESS, payload: response.data });
+    } catch (error) {
+      dispatch({ type: CREATE_USER_FAILURE, payload: error.message });
+    }
+  };
+
+export const searchByName = (name) => {
+  return async (dispatch) => {
+    try {
+      const apiData = await axios.get(`http://localhost:3000/products/search/${name}`);
+      const searchName = apiData.data;
+      return dispatch({
+        type: GET_SEARCH_BY_NAME,
+        payload: searchName,
+      });
+    } catch (e) {
+      console.log(e.response.data);
+      return dispatch({
+        type: NO_EVENTS,
+        payload: e.response.data,
+      });
+    }
+  };
+};
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,1 +1,68 @@
-///dsadsad
+import {
+  SET_USER_DATA,
+  CLEAR_USER_DATA,
+  CREATE_USER_REQUEST,
+  CREATE_USER_SUCCESS,
+  CREATE_USER_FAILURE,
+  NO_EVENTS,
+  GET_SEARCH_BY_NAME,
+
+} from "../Actions_Type/actions_type";
+
+
+const initialState = {
+  userDataSession: null,
+  loading: false,
+  error: null,
+  newUser: null,
+  noEvents: "",
+  searchName: [],
+
+};
+
+
+const userDataReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER_DATA:
+      return {
+        ...state,
+        userDataSession: action.payload,
+      };
+    case CLEAR_USER_DATA:
+      return {
+        ...state,
+        userDataSession: null,
+      };
+
+      case CREATE_USER_REQUEST:
+        return {
+          ...state,
+          loading: true,
+          error: null,
+        };
+      case CREATE_USER_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          newUser: action.payload,
+          error: null,
+        };
+      case CREATE_USER_FAILURE:
+        return {
+          ...state,
+          loading: false,
+          error: action.payload,
+        };
+      case GET_SEARCH_BY_NAME:
+        return {
+          ...state,
+          searchName: action.payload,
+          noEvents: "",
+        };
+
+    default:
+      return state;
+  }
+};
+
+export default userDataReducer;
