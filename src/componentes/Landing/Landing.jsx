fda5684e8d88717bb77a1d589b068178ec93689e
AAlexandraCastillo <casstlealexandra@gmail.com>
2024-02-07 22:09:48 -0300

    land

--- src/componentes/Landing/Landing.jsx
+++ src/componentes/Landing/Landing.jsx
@@ -1,6 +1,6 @@
 
 import style from './Landing.module.css'
-import ImageGallery from '../Images/ImageGallery' 
+import ImageGallery from '../ImageGallery/ImageGallery' 
 const Landing=()=>{
  const images=['https://tse1.mm.bing.net/th?id=OIP.gy0hB8b8-0YvJ3ODBAql6QHaEo&pid=Api&P=0&h=180','https://image.freepik.com/foto-gratis/amigos-divirtiendose-alrededor-fogata-noche_109710-4520.jpg','https://tse3.mm.bing.net/th?id=OIP.1-a6tn9mc0gwS1c2OfR-9gHaE7&pid=Api&P=0&h=180','https://tse1.mm.bing.net/th?id=OIP.RP5Eh4uosoD6ZgnX2cqDAwHaFj&pid=Api&P=0&h=180','https://www.interpatagonia.com/plantillas/grandes/33579-00Gr.jpg','https://casasrusticas.org/wp-content/uploads/2021/12/cabanas-rusticas-de-piedra-y-madera-01-1024x683.jpg','https://images.mirai.com/INFOROOMS/100120276/09Tb4L1tXTdhiaVkF2UT/09Tb4L1tXTdhiaVkF2UT_large.jpg','https://i.pinimg.com/originals/ce/8d/fe/ce8dfe3b7a548e752b95085e8766e551.jpg']
      return(
@@ -19,7 +19,7 @@ const Landing=()=>{
     <div class="row">
       <div class="col-md-12">
         <p>Derechos de autor © 2024. Todos los derechos reservados.        </p>
-        <h1> .contactanos:  0800-345-678</h1>
+        
       </div>
     </div>
   </div>
