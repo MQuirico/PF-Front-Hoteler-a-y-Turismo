a9a3e91f983e36d889b226cf142a1853fa2c45ae
Joan Laurente <Joan2022Laurente>
2024-02-08 11:40:33 -0500

    home diseño

--- src/views/Home/home.jsx
+++ src/views/Home/home.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React from "react";
+import Card from "../../componentes/Card/card";
+import data from "../../componentes/Card/data.json";
+import "./home.css";
 
 function Home() {
   console.log("Renderizando el componente Home");
-  
-  return (
-    <div>
-      <header>
-        <h1>Bienvenido a nuestra página de inicio</h1>
-        <nav>
-          <ul>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Acerca de</a></li>
-            <li><a href="#">Contacto</a></li>
-          </ul>
-        </nav>
-      </header>
 
-      <section className="main-content">
-        <h2>¡Hola mundo!</h2>
-        <p>Este es un ejemplo de página de inicio.</p>
+  return (
+    <div className="homeView">
+      <section className="mainContent">
+        <div className="title">
+          <h2>Home</h2>
+          <p>Este es un ejemplo de página de inicio.</p>
+        </div>
+
+        <div className="containerr">
+          <div className="row">
+            {data.map((cabaña) => (
+              <div className="box" key={cabaña.id}>
+                <Card cabaña={cabaña} />
+              </div>
+            ))}
+          </div>
+        </div>
       </section>
 
       <footer>
