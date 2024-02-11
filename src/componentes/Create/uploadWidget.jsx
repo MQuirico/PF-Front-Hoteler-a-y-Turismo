fa1678bb4536ae6e641a153e90d1d8f995106b84
MQuirico <128657405+MQuirico@users.noreply.github.com>
2024-02-11 02:17:36 -0300

    Formulario alta productos + Cloudinary implementado

---
+++ src/componentes/Create/uploadWidget.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef, useContext } from 'react';
+import { formContext } from './create';
+const images = []
+const UploadWidget = () =>{
+    const cloudinaryRef = useRef();
+    const widgetRef = useRef();
+    const {setValue} = useContext(formContext)
+    useEffect(() =>{
+        cloudinaryRef.current = window.cloudinary;
+        widgetRef.current = cloudinaryRef.current.createUploadWidget({
+            cloudName: "ds4blfuip",
+            uploadPreset: "ml_default"
+        }, function(error, result){
+            if (!error && result && result.event === "success") {
+                console.log("Imagen subida exitosamente:", result.info.secure_url);
+                images.push(result.info.secure_url)  
+                console.log('Estas son las url de las imagenes:', images) 
+                setValue('images', images)
+            } else {
+                console.error("Error al subir la imagen:", error);
+            }
+        });
+        
+       
+    }, []);
+
+    return (
+        <button onClick={() => widgetRef.current.open()}>
+            Subir imágenes
+        </button>
+    );
+}
+
+export default UploadWidget;
